c65c961dcf7e22544db5da8c0fe8b7cdab6d0189
Vu Duc Hoai Nam <vinhoainam0815@gmail.com>
2025-10-23 13:25:27 +0700

    config tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import "./assets/css/App.scss";
+
 function App() {
-	return <p>xin chào</p>;
+	return (
+		<div className="flex justify-center items-center mt-10">
+			<h1 className="text-13 font-bold underline">xin chào</h1>
+		</div>
+	);
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import "./assets/css/App.scss";
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
